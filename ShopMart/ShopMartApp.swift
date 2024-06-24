b6430a5310b1b04e051a13be89a6c3ca7e6a6a96
ravindu <rmahiti2@gmail.com>
2024-06-25 00:27:05 +0530

    Initial Commit

---
+++ ShopMart/ShopMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopMartApp.swift
+//  ShopMart
+//
+//  Created by Ravindu Mahiti on 2024-06-25.
+//
+
+import SwiftUI
+
+@main
+struct ShopMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
